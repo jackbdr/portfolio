15a21b374a391fbfdc235adca66bf95660c7fd29
Jack <jackbdrobinson@gmail.com>
2022-09-13 16:07:26 +0100

    navbar selection fade

--- user/src/components/NavBar.js
+++ user/src/components/NavBar.js
@@ -6,27 +6,32 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 
 import Logo from '../images/fresh1.gif'
+import In from '../images/linkedin.png'
+import Github from '../images/github.png'
 import Mail from '../images/mail.png'
 
 const NavBar = () => {
 
   return (
-    <Navbar expand='sm' className='navbar'>
-      <Container className='navbar-wrap'>
-        {/* <Navbar.Brand> */}
-        <img src={Logo} className='logo' />
-        {/* </Navbar.Brand> */}
-        <div className='bulk-navbar'>
-          <p className='about-btn'>about</p>
-          <p className='projects-btn'>projects</p>
-          <p className='exp-btn'>experience</p>
-          <p className='interests-btn'>interests</p>
-        </div>
-        {/* <div className='contact-div'> */}
-        <img className='mail-btn' src={Mail} />
-        {/* </div> */}
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand='sm' className='navbar'>
+        <Container className='navbar-wrap'>
+          {/* <Navbar.Brand> */}
+          <img src={Logo} className='logo' />
+          {/* </Navbar.Brand> */}
+          <div className='bulk-navbar'>
+            <p className='about-btn fadeout'>about</p>
+            <p className='projects-btn fadeout'>projects</p>
+            <p className='exp-btn fadeout'>experience</p>
+            <p className='interests-btn fadeout'>interests</p>
+          </div>
+          {/* <div className='media-div'> */}
+          <img className='mail-btn' src={Mail} alt='email' />
+          {/* </div> */}
+        </Container>
+      </Navbar>
+      <hr />
+    </>
   )
 
 }
