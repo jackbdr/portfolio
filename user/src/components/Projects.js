3d313e4442c2fdcae7e92f1cb545dc6e79e4c5a2
Jack <jackbdrobinson@gmail.com>
2022-10-10 11:27:50 +0100

    proj imgs

--- user/src/components/Projects.js
+++ user/src/components/Projects.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
+// import proj1img from '../images/'
+
 const Projects = () => {
 
   return (
     <section id='projects'>
       <div className='projects-content'>
         <h2 className='projects-head section-head'>Projects</h2>
+        <div className='project-div'>
+          {/* <img src={}/> */}
+          <div className='project-info'>
+            <h3 className='project-name'>What On Earth</h3>
+
+          </div>
+        </div>
       </div>
     </section>
   )
