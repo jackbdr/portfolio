3eaf9e2d2ad7e61383c76ae2eb70b2f43aadd03e
Jack <jackbdrobinson@gmail.com>
2022-10-13 14:52:42 +0100

    exp begin

--- user/src/components/Experience.js
+++ user/src/components/Experience.js
@@ -4,7 +4,12 @@ const Experience = () => {
 
   return (
     <section id='experience'>
-      
+      <div className='experience-content flex-center'>
+        <h2 className='experience-head section-head'>Experience</h2>
+        <div className='timeline'>
+
+        </div>
+      </div>
     </section>
   )
 }
