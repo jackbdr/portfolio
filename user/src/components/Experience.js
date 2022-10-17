ba94c00d1fb944e21354d430a895047001003906
Jack <jackbdrobinson@gmail.com>
2022-10-17 12:11:03 +0100

    custom accordion begin

--- user/src/components/Experience.js
+++ user/src/components/Experience.js
@@ -1,28 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import { useAccordionButton } from 'react-bootstrap/AccordionButton'
+import Card from 'react-bootstrap/Card'
 
 const Experience = () => {
+
+  const CustomToggle = ({ children, eventKey }) => {
+    const decoratedOnClick = useAccordionButton(eventKey, () => {
+      console.log('custom!')
+    })
+
+    return (
+      <button
+        type='button'
+        className='custom-acc-header'
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </button>
+    )
+  }
 
   return (
     <section id='experience'>
       <div className='experience-content flex-center'>
         <h2 className='experience-head section-head'>Experience</h2>
-        <div className='timeline'>
-          <p>UoS</p>
-          <p>{'>>'}</p>
-          <p>LKGD</p>
-          <p>{'>>'}</p>
-          <p>CELTA</p>
-          <p>{'>>'}</p>
-          <p>TEFL</p>
-          <p>{'>>'}</p>
-          <p>SCHub</p>
-          <p>{'>>'}</p>
-          <p>FF</p>
-          <p>{'>>'}</p>
-          <p>GA</p>
-          <p>{'>>'}</p>
-          <p>HGF</p>
-        </div>
+        <Accordion className='exp-accordion' defaultActiveKey="0" flush>
+          <Card eventKey="0">
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Card>
+          <Card eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Card>
+        </Accordion>
       </div>
     </section>
   )
