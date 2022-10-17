c3aa57cd95d90d7e687f8e2bbac89c59757e4c01
Jack <jackbdrobinson@gmail.com>
2022-10-17 10:05:36 +0100

    experience begin

--- user/src/components/Experience.js
+++ user/src/components/Experience.js
@@ -7,7 +7,21 @@ const Experience = () => {
       <div className='experience-content flex-center'>
         <h2 className='experience-head section-head'>Experience</h2>
         <div className='timeline'>
-
+          <p>UoS</p>
+          <p>{'>>'}</p>
+          <p>LKGD</p>
+          <p>{'>>'}</p>
+          <p>CELTA</p>
+          <p>{'>>'}</p>
+          <p>TEFL</p>
+          <p>{'>>'}</p>
+          <p>SCHub</p>
+          <p>{'>>'}</p>
+          <p>FF</p>
+          <p>{'>>'}</p>
+          <p>GA</p>
+          <p>{'>>'}</p>
+          <p>HGF</p>
         </div>
       </div>
     </section>
