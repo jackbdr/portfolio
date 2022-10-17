460694397424a7d1d0b783144ba102e194993f2e
Jack <jackbdrobinson@gmail.com>
2022-10-17 19:51:16 +0100

    plus minus signs

--- user/src/components/Experience.js
+++ user/src/components/Experience.js
@@ -5,51 +5,56 @@ import Card from 'react-bootstrap/Card'
 
 const Experience = () => {
 
-  const CustomToggle = ({ children, eventKey }) => {
+  const CustomToggle = ({ children, eventKey, handleClick }) => {
     const decoratedOnClick = useAccordionButton(eventKey, () => {
-      console.log('custom!')
+      handleClick()
     })
 
     return (
-      <button
+      <div
         type='button'
         className='custom-acc-header'
         onClick={decoratedOnClick}
       >
         {children}
-      </button>
+      </div>
     )
   }
+
+  const [activeKey, setActiveKey] = useState(0)
+
+  const data = [
+    { jobTitle: 'Carpenter\'s Assistant', descrip: 'worked on projects ranging from...' },
+    { jobTitle: 'Team Leader / Warehouse Manager', descrip: 'Originally volunteered during the first lockdown before being employed full-time. Responsible for running foodbank sessions' },
+    { jobTitle: 'Freddie\'s Flowers Rider', descrip: 'Delivering flowers via cargo bike to subscribed customers.' }
+  ]
 
   return (
     <section id='experience'>
       <div className='experience-content flex-center'>
         <h2 className='experience-head section-head'>Experience</h2>
-        <Accordion className='exp-accordion' defaultActiveKey="0" flush>
-          <Card eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Card>
-          <Card eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Card>
+        <Accordion className='' defaultActiveKey={0} activeKey={activeKey}>
+          {data.map((item, index) => (
+            <Card key={index}>
+              <CustomToggle
+                as={Card.Header}
+                eventKey={index}
+                handleClick={() => {
+                  if (activeKey === index) {
+                    setActiveKey(null)
+                  } else {
+                    setActiveKey(index)
+                  }
+                }}
+              >
+                {item.jobTitle}
+                {activeKey === index ? '-' : '+'}
+              </CustomToggle>
+              <Accordion.Collapse eventKey={index}>
+                <Card.Body>{item.descrip}</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          ))}
         </Accordion>
       </div>
     </section>
